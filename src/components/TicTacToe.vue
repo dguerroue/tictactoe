eb4f99c2e563e7743528ade3c285518a2aa617d4
dimitri guerroue <dimitri.guerroue@gmail.com>
2019-01-09 17:47:28 +0100

    add fonction checkWin

--- src/components/TicTacToe.vue
+++ src/components/TicTacToe.vue
@@ -29,6 +29,7 @@ export default {
       player: 'circle', //'cross' or 'circle'
       iswin: false,
       cases: {
+        case0: null,
         case1: null,
         case2: null,
         case3: null,
@@ -37,7 +38,6 @@ export default {
         case6: null,
         case7: null,
         case8: null,
-        case9: null,
       }
     }
   },
@@ -47,13 +47,12 @@ export default {
       if(value === null && this.iswin == false){
         this.cases[index] = this.player;
 
-          this.changePlayer();
+          this.checkWin();
+          if(this.iswin == false){
+
+            this.changePlayer();
+          }
       }
-
-      if(this.checkWin()){
-
-      }
-
     },
     changePlayer(){
       this.player = this.player == 'circle' ? 'cross' : 'circle';
@@ -62,11 +61,42 @@ export default {
       // let player = this.player;
       let iswin = null;
 
-      if(this.cases.case1 == 'circle' && this.cases.case2 == 'circle' && this.cases.case3 == 'circle'){
+      //check player circle
+      if(this.cases.case0 == 'circle' && this.cases.case1 == 'circle' && this.cases.case2 == 'circle'){
         this.iswin = true;
-        this.player="circle";
-      } else{
-        this.iswin = false;
+      } else if (this.cases.case3 == 'circle' && this.cases.case4 == 'circle' && this.cases.case5 == 'circle') {
+        this.iswin = true;
+      } else if (this.cases.case6 == 'circle' && this.cases.case7 == 'circle' && this.cases.case8 == 'circle') {
+        this.iswin = true;
+      } else if (this.cases.case0 == 'circle' && this.cases.case3 == 'circle' && this.cases.case6 == 'circle') {
+        this.iswin = true;
+      } else if (this.cases.case1 == 'circle' && this.cases.case4 == 'circle' && this.cases.case7 == 'circle') {
+        this.iswin = true;
+      } else if (this.cases.case2 == 'circle' && this.cases.case5 == 'circle' && this.cases.case8 == 'circle') {
+        this.iswin = true;
+      } else if (this.cases.case0 == 'circle' && this.cases.case4 == 'circle' && this.cases.case8 == 'circle') {
+        this.iswin = true;
+      } else if (this.cases.case2 == 'circle' && this.cases.case4 == 'circle' && this.cases.case6 == 'circle') {
+        this.iswin = true;
+      }
+
+      //check player cross
+      if(this.cases.case0 == 'cross' && this.cases.case1 == 'cross' && this.cases.case2 == 'cross'){
+        this.iswin = true;
+      } else if (this.cases.case3 == 'cross' && this.cases.case4 == 'cross' && this.cases.case5 == 'cross') {
+        this.iswin = true;
+      } else if (this.cases.case6 == 'cross' && this.cases.case7 == 'cross' && this.cases.case8 == 'cross') {
+        this.iswin = true;
+      } else if (this.cases.case0 == 'cross' && this.cases.case3 == 'cross' && this.cases.case6 == 'cross') {
+        this.iswin = true;
+      } else if (this.cases.case1 == 'cross' && this.cases.case4 == 'cross' && this.cases.case7 == 'cross') {
+        this.iswin = true;
+      } else if (this.cases.case2 == 'cross' && this.cases.case5 == 'cross' && this.cases.case8 == 'cross') {
+        this.iswin = true;
+      } else if (this.cases.case0 == 'cross' && this.cases.case4 == 'cross' && this.cases.case8 == 'cross') {
+        this.iswin = true;
+      } else if (this.cases.case2 == 'cross' && this.cases.case4 == 'cross' && this.cases.case6 == 'cross') {
+        this.iswin = true;
       }
 
       // let checkCombinaisons = [
